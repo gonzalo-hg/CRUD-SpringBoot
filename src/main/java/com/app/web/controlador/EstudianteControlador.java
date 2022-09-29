dc7d61f1254a4eb1cefd50aee074e10795d91684
gonzalohg <gonzalo.hg19@gmail.com>
2022-09-29 00:20:25 -0500

    Se agrega controladorEstudiante

--- src/main/java/com/app/web/controlador/EstudianteControlador.java
+++ src/main/java/com/app/web/controlador/EstudianteControlador.java
@@ -2,18 +2,21 @@ package com.app.web.controlador;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.app.web.repositorio.EstudianteRepositorio;
+import com.app.web.servicio.EstudianteServicio;
 
 @Controller
 public class EstudianteControlador {
 
 	@Autowired
-	private EstudianteRepositorio repositorio;
+	private EstudianteServicio servicio;
 	
 	@GetMapping({"/estudiantes", "/"})
-	public String listarEstudiantes() {
+	public String listarEstudiantes(Model model) {
+		model.addAttribute("estudiante",servicio.listarTodosEstudiantes());
 		return "estudiantes";//retorna al archivo estudiantes
 	}
 }
